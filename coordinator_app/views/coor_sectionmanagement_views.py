e0a88c125760d91ceb2029ae1c3730e88c6fa18d
HiitsMehayst <eh202201320@wmsu.edu.ph>
2026-01-21 16:18:11 +0800

    update
    update

--- coordinator_app/views/coor_sectionmanagement_views.py
+++ coordinator_app/views/coor_sectionmanagement_views.py
@@ -19,20 +19,48 @@ def section_management(request):
             # Update all section counts from database to ensure accuracy
             for section in sections:
                 section.update_current_students_count()
+            
+            # For REGULAR program, separate sections by track
+            top5_sections = []
+            hetero_sections = []
+            other_sections = []
+            
+            if program.code == 'REGULAR':
+                for section in sections:
+                    if section.regular_track == 'TOP5':
+                        top5_sections.append(section)
+                    elif section.regular_track == 'HETERO':
+                        hetero_sections.append(section)
+                    else:
+                        other_sections.append(section)
+            else:
+                # For non-REGULAR programs, just use all sections
+                other_sections = list(sections)
         else:
             sections = Section.objects.none()
+            top5_sections = []
+            hetero_sections = []
+            other_sections = []
         
         context = {
             'user': request.user,
             'program': program,
             'sections': sections,
+            'top5_sections': top5_sections,
+            'hetero_sections': hetero_sections,
+            'other_sections': other_sections,
             'user_profile': user_profile,
+            'is_regular_program': program.code == 'REGULAR' if program else False,
         }
         return render(request, 'coordinator_app/section_management.html', context)
     except Exception as e:
         context = {
             'user': request.user,
             'sections': [],
+            'top5_sections': [],
+            'hetero_sections': [],
+            'other_sections': [],
+            'is_regular_program': False,
             'error': str(e)
         }
         return render(request, 'coordinator_app/section_management.html', context)
